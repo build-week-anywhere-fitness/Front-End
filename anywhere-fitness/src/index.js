1802773f6833eb01dc8b60e50e6cb17e8561f6c6
cwern40 <chris.wernli@hotmail.com>
2019-07-25 18:16:19 -0600

    I created the reducer in the reducers/index.js file and created the store in the index.js file. I also created the InstructorLogin.js file

--- anywhere-fitness/src/index.js
+++ anywhere-fitness/src/index.js
@@ -4,7 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from "react-redux"
-import store from "./components/store/store"
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import reducer from './components/reducers/index';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 const app = (
     <Provider store={store}>
