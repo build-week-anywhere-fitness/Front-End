c3d4f7e5f4031a15897ace3560f379415cfdf073
cwern40 <chris.wernli@hotmail.com>
2019-07-29 16:23:36 -0600

    I created the instructor register actions in the InstructorActions.js file

--- anywhere-fitness/src/components/actions/InstructorActions.js
+++ anywhere-fitness/src/components/actions/InstructorActions.js
@@ -4,6 +4,10 @@ export const INSTRUCTOR_LOGIN_START = 'INSTRUCTOR_LOGIN_START'
 export const INSTRUCTOR_LOGIN_SUCCESS = 'INSTRUCTOR_LOGIN_SUCCESS'
 export const INSTRUCTOR_LOGIN_FAILED = 'INSTRUCTOR_LOGIN_FAILED'
 
+export const INSTRUCTOR_REGISTER_START = 'INSTRUCTOR_REGISTER_START'
+export const INSTRUCTOR_REGISTER_SUCCESS = 'INSTRUCTOR_REGISTER_SUCCESS'
+export const INSTRUCTOR_REGISTER_FAILED = 'INSTRUCTOR_REGISTER_FAILED'
+
 const url = 'https://anywhere-fitness-azra-be.herokuapp.com'
 
 export function instructorLogin(username, password) {
@@ -18,6 +22,22 @@ export function instructorLogin(username, password) {
             .catch((err) => {
                 console.log(err)
                 dispatch({ type: INSTRUCTOR_LOGIN_FAILED })
+            })
+    }
+}
+
+export function instructorRegister(fullname, username, password) {
+    return (dispatch) => {
+        dispatch({ type:INSTRUCTOR_REGISTER_START })
+
+        return axios.post(`${url}/api/auth/register`, { fullname, username, password })
+            .then((res) => {
+                console.log(res);
+                dispatch({ type: INSTRUCTOR_REGISTER_SUCCESS })
+            })
+            .catch((err) => {
+                console.log(err);
+                dispatch({ type: INSTRUCTOR_REGISTER_FAILED })
             })
     }
 }
